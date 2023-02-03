9b4119720ef82f88ea679ea5bc287336d2bc1078
Jake <Jake.Laoyu@gmail.com>
2023-02-03 22:25:51 +0800

    2023-02-03 22:25:51

---
+++ script/backup.js
@@ -0,0 +1,10 @@
+const execSync = require("child_process").execSync;
+const dayjs = require('dayjs')
+
+console.log('Start backup')
+
+execSync("git add .")
+execSync(`git commit -m '${dayjs().format('YYYY-MM-DD HH:mm:ss')}'`)
+execSync("git push")
+
+console.log("Backup completed")
