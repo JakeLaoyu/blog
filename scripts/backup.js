26214c34365588a66974c36dfa77bfcca59703c1
Jake <Jake.Laoyu@gmail.com>
2023-02-03 22:11:51 +0800

    2023-02-03 22:11:51

---
+++ scripts/backup.js
@@ -0,0 +1,18 @@
+const exec = require("child_process").exec;
+const dayjs = require("dayjs");
+
+const execPromise = (command) => {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(stdout);
+            }
+        });
+    });
+}
+
+execPromise("git add .")
+    .then(() => execPromise(`git commit -m '${dayjs().format('YYYY-MM-DD HH:mm:ss')}'`))
+    .then(() => execPromise("git push"))
