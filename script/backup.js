8be08c3761fe92e6d0435bc363b2bf1ed19e7746
Jake <Jake.Laoyu@gmail.com>
2023-02-04 00:49:17 +0800

    2023-02-04 00:49:17

--- script/backup.js
+++ script/backup.js
@@ -5,6 +5,7 @@ console.log('Start backup')
 
 execSync("git add .")
 execSync(`git commit -m '${dayjs().format('YYYY-MM-DD HH:mm:ss')}'`)
+console.log('start push')
 execSync("git push")
 
 console.log("Backup completed")
