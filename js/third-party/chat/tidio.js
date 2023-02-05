fd82166ea0c52ecd8025b898fd2a8a90122cb518
JakeLaoyu <JakeLaoyu@users.noreply.github.com>
2023-02-05 04:46:06 +0000

    deploy: 0d03268e782403008a6970b7dddc8ed842711786

--- js/third-party/chat/tidio.js
+++ js/third-party/chat/tidio.js
@@ -1,1 +1,1 @@
-!function(){var t=document.querySelector(".sidebar-button button");t&&t.addEventListener("click",(()=>{tidioChatApi.open()}))}();
+!function(){var t=document.querySelector(".sidebar-button button");t&&t.addEventListener("click",()=>{tidioChatApi.open()})}();
